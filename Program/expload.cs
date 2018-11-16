339a67957298c893ae5590ff6469eb8afa15fdfa
dimajedi <Axerace37918>
2018-11-16 23:15:58 +0300

    contract init

---
+++ Program/expload.cs
@@ -0,0 +1,14 @@
+namespace Expload {
+
+    using Pravda;
+    using System;
+
+    [Program]
+    public class expload {
+        public static void Main() { }
+
+        public string HelloWorld() {
+            return "Hello, world!";
+        }
+    }
+}
