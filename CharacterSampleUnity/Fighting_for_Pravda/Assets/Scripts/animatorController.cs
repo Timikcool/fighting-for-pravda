f5f8f462e8939fbfb4ed70e4b8879c2482f5cf89
Paul White <theflashofsonic@gmail.com>
2018-11-17 15:32:16 +0300

    script animator_controller

---
+++ CharacterSampleUnity/Fighting_for_Pravda/Assets/Scripts/animatorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
