f97bba8b07e4c1734971f016db52bc5da3240e1f
Paul White <theflashofsonic@gmail.com>
2018-11-17 16:14:25 +0300

    Script animation controller making

--- CharacterSampleUnity/Fighting_for_Pravda/Assets/Scripts/animatorController.cs
+++ CharacterSampleUnity/Fighting_for_Pravda/Assets/Scripts/animatorController.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class animatorController : MonoBehaviour {
 
+	public Animator anim;
+
+	public void punch0() {
+		anim.setTrigger("isPunching0");
+	}
+
+	public void punch1() {
+		anim.setTrigger("isPunching1");
+	}
+
+	public void kick0() {
+		anim.setTrigger("isKicking0");
+	}
+
+	public void kick1() {
+		anim.setTrigger("isKicking1");
+	}
+
+	public void dodge() {
+		anim.setTrigger("isDodging");
+	}
+
+	public void getDamage() {
+		anim.setTrigger("isGettingDamage");
+	}
+
+	public void die() {
+		anim.setTrigger("isDying");
+	}
+
+
+
 	// Use this for initialization
 	void Start () {
 		
